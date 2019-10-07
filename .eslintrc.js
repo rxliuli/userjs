e454843af85633d2d94a38d80aa91e04c08ec053
rxliuli <rxliuli@gmail.com>
2019-10-07 14:03:12 +0800

    在 eslint 配置中添加 GM_* 全局变量

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,9 +4,22 @@ module.exports = {
     browser: true,
   },
   globals: {
-    //油猴全局变量
-    unsafeWindow: 'writable',
     //引用的工具函数库
     rx: 'readonly',
+    //油猴全局变量
+    unsafeWindow: 'writable',
+    GM_registerMenuCommand: 'readonly',
+    GM_setValue: 'readonly',
+    GM_getValue: 'readonly',
+    GM_deleteValue: 'readonly',
+    GM_addStyle: 'readonly',
+    GM_getResourceText: 'readonly',
+    GM_getResourceURL: 'readonly',
+    GM_info: 'readonly',
+    GM_listValues: 'readonly',
+    GM_log: 'readonly',
+    GM_openInTab: 'readonly',
+    GM_setClipboard: 'readonly',
+    GM_xmlhttpRequest: 'readonly',
   },
 }
