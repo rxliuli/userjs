2fbd528dfdd174f8b65533f8202e8cf9134659d1
rxliuli <rxliuli@gmail.com>
2020-06-19 23:26:35 +0800

    [Slack 暗黑模式萌化（动态泡水灵梦）] 更新 0.1.1，修复 slack 更新引起的主题错误

--- src/SlackDarkCute/SlackDarkCute.user.js
+++ src/SlackDarkCute/SlackDarkCute.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Slack 暗黑模式萌化（动态泡水灵梦）
 // @namespace    http://github.com/rxliuli/userjs
-// @version      0.1.0
+// @version      0.1.1
 // @description  try to take over the world!
 // @author       rxliuli
 // @match        https://app.slack.com/client/*
@@ -31,50 +31,57 @@
 `);
     // language=CSS
     GM_addStyle(`video#videoWallPaper {
-  position: fixed;
-  right: 0;
-  bottom: 0;
+      position: fixed;
+      right: 0;
+      bottom: 0;
 
-  min-width: 100%;
-  min-height: 100%;
+      min-width: 100%;
+      min-height: 100%;
 
-  width: auto;
-  height: auto;
-  z-index: -100;
+      width: auto;
+      height: auto;
+      z-index: -100;
 
-  background-image: url(https://cdn.jsdelivr.net/gh/rxliuli/img-bed/20200306083232.jpg);
-  background-repeat: no-repeat;
-  background-size: cover;
-}
-/*需要透明化的元素*/
-body,
-.p-client_container,
-.p-client,
-.p-client .p-top_nav,
-.p-workspace {
-  background-color: transparent;
-}
-.p-workspace {
-  /*background-image: url(https://cdn.jsdelivr.net/gh/rxliuli/img-bed/20200306083232.jpg);*/
-  /*background-repeat: no-repeat;*/
-  /*background-size: cover;*/
-}
+      /*background-image: url(https://cdn.jsdelivr.net/gh/rxliuli/img-bed/20200306083232.jpg);*/
+      background-repeat: no-repeat;
+      background-size: cover;
+  }
+  /*需要透明化的元素*/
+  body,
+  .p-client_container,
+  .p-client,
+  .p-client .p-top_nav,
+  .p-workspace {
+      background-color: transparent;
+  }
+  .p-workspace {
+      /*background-image: url(https://cdn.jsdelivr.net/gh/rxliuli/img-bed/20200306083232.jpg);*/
+      /*background-repeat: no-repeat;*/
+      /*background-size: cover;*/
+  }
 
-/* 工作区 */
-.p-workspace * {
-  background-color: rgba(48, 48, 48, 0.08) !important;
-}
-/* 鼠标在消息上 */
-.c-message_kit__message:hover {
-  background-color: rgba(48, 48, 48, 0.5) !important;
-}
-/* 消除文本特殊加重 */
-.c-message_kit__blocks--rich_text * {
-  background-color: transparent !important;
-}
-.c-message_list__day_divider__line {
-  border-color: rgba(48, 48, 48, 0.5) !important;
-}
-`);
+  /* 工作区 */
+  .p-workspace *,
+  .p-workspace__channel_sidebar *,
+  .p-workspace__primary_view *,
+  .p-workspace__secondary_view *,
+  .c-virtual_list__item * {
+      background-color: rgba(48, 48, 48, 0.08) !important;
+  }
+  /* 鼠标在消息上 */
+  .c-message_kit__message:hover {
+      background-color: rgba(48, 48, 48, 0.5) !important;
+  }
+  /* 消除文本特殊加重 */
+  .c-message_kit__blocks--rich_text * {
+      background-color: transparent !important;
+  }
+  .c-message_list__day_divider__line {
+      border-color: rgba(48, 48, 48, 0.5) !important;
+  }
+  .p-top_nav__search{
+      background-color: rgba(48, 48, 48, 0.08) !important;
+  }
+  `);
     document.body.appendChild($videoEl);
 })();
