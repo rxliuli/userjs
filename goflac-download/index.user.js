157628bb361698f8ebbdb45da42a95fd2d6e8a90
rxliuli <rxliuli@gmail.com>
2022-01-31 12:26:50 +0800

    Updates gh-pages by liuli-cli

--- goflac-download/index.user.js
+++ goflac-download/index.user.js
@@ -2,46 +2,18 @@
 // @name        goflac-download
 // @description 简化下载音乐需要重命名的麻烦，基本上和 91flac-download 差不多
 // @namespace   http://github.com/rxliuli/userjs
-// @version     0.1.0
+// @version     0.1.1
 // @author      rxliuli
 // @match       https://www.goflac.com/*
 // @license     MIT
 // @grant       GM_xmlhttpRequest
 // ==/UserScript==
-
 (function () {
   'use strict';
 
-  /*! *****************************************************************************
-  Copyright (c) Microsoft Corporation.
+  var y=(l,e,t)=>new Promise((n,o)=>{var r=i=>{try{p(t.next(i));}catch(c){o(c);}},a=i=>{try{p(t.throw(i));}catch(c){o(c);}},p=i=>i.done?n(i.value):Promise.resolve(i.value).then(r,a);p((t=t.apply(l,e)).next());});var g;(function(n){n.Xlsx="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",n.Zip="application/zip",n.Pdf="application/pdf";})(g||(g={}));var s=class{static dataURItoBlob(e){let t=atob(e.split(",")[1]),n=e.split(",")[0].split(":")[1].split(";")[0],o=new ArrayBuffer(t.length),r=new Uint8Array(o);for(let a=0;a<t.length;a++)r[a]=t.charCodeAt(a);return new Blob([o],{type:n})}static blobToFile(e,t){let n=e;return n.lastModifiedDate=new Date,n.name=t,e}static arrayBufferToBlob(e,t){return new Blob([new Uint8Array(e).buffer],t)}static initLookup(){let e=new Uint8Array(256);for(let t=0;t<s.chars.length;t++)e[s.chars.charCodeAt(t)]=t;return e}static arrayBufferToBase64(e){let t=new Uint8Array(e),n,o=t.length,r="";for(n=0;n<o;n+=3)r+=s.chars[t[n]>>2],r+=s.chars[(t[n]&3)<<4|t[n+1]>>4],r+=s.chars[(t[n+1]&15)<<2|t[n+2]>>6],r+=s.chars[t[n+2]&63];return o%3==2?r=r.substring(0,r.length-1)+"=":o%3==1&&(r=r.substring(0,r.length-2)+"=="),r}static base64ToArrayBuffer(e){let t=e.length*.75,n=e.length,o,r=0,a,p,i,c;e[e.length-1]==="="&&(t--,e[e.length-2]==="="&&t--);let m=new ArrayBuffer(t),f=new Uint8Array(m);for(o=0;o<n;o+=4)a=s.lookup[e.charCodeAt(o)],p=s.lookup[e.charCodeAt(o+1)],i=s.lookup[e.charCodeAt(o+2)],c=s.lookup[e.charCodeAt(o+3)],f[r++]=a<<2|p>>4,f[r++]=(p&15)<<4|i>>2,f[r++]=(i&3)<<6|c&63;return m}static arrayBufferToString(e){return new Promise((t,n)=>{let o=new Blob([e]),r=new FileReader;r.addEventListener("load",()=>t(r.result)),r.addEventListener("error",n),r.readAsText(o,"utf-8");})}static arrayBufferToJson(e){return y(this,null,function*(){return JSON.parse(yield this.arrayBufferToString(e))})}},v=s;v.chars="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/",v.lookup=s.initLookup(),v.ArrayBufferToJson=s.arrayBufferToJson;function K(l,e="unknown"){let t=document.createElement("a");t.download=e,t.style.display="none",t.href=URL.createObjectURL(l),document.body.appendChild(t),t.click(),document.body.removeChild(t);}
 
-  Permission to use, copy, modify, and/or distribute this software for any
-  purpose with or without fee is hereby granted.
-
-  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
-  REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
-  AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
-  INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
-  LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
-  OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
-  PERFORMANCE OF THIS SOFTWARE.
-  ***************************************************************************** */function t(e,t,r,n){return new(r||(r=Promise))((function(o,s){function a(e){try{l(n.next(e));}catch(e){s(e);}}function i(e){try{l(n.throw(e));}catch(e){s(e);}}function l(e){var t;e.done?o(e.value):(t=e.value,t instanceof r?t:new r((function(e){e(t);}))).then(a,i);}l((n=n.apply(e,t||[])).next());}))}var r;!function(e){e.Xlsx="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",e.Zip="application/zip",e.Pdf="application/pdf";}(r||(r={}));class n{static dataURItoBlob(e){const t=atob(e.split(",")[1]),r=e.split(",")[0].split(":")[1].split(";")[0],n=new ArrayBuffer(t.length),o=new Uint8Array(n);for(let e=0;e<t.length;e++)o[e]=t.charCodeAt(e);return new Blob([n],{type:r})}static blobToFile(e,t){const r=e;return r.lastModifiedDate=new Date,r.name=t,e}static arrayBufferToBlob(e,t){return new Blob([new Uint8Array(e).buffer],t)}static initLookup(){const e=new Uint8Array(256);for(let t=0;t<n.chars.length;t++)e[n.chars.charCodeAt(t)]=t;return e}static arrayBufferToBase64(e){let t,r=new Uint8Array(e),o=r.length,s="";for(t=0;t<o;t+=3)s+=n.chars[r[t]>>2],s+=n.chars[(3&r[t])<<4|r[t+1]>>4],s+=n.chars[(15&r[t+1])<<2|r[t+2]>>6],s+=n.chars[63&r[t+2]];return o%3==2?s=s.substring(0,s.length-1)+"=":o%3==1&&(s=s.substring(0,s.length-2)+"=="),s}static base64ToArrayBuffer(e){let t=.75*e.length;const r=e.length;let o,s,a,i,l,c=0;"="===e[e.length-1]&&(t--,"="===e[e.length-2]&&t--);const u=new ArrayBuffer(t),h=new Uint8Array(u);for(o=0;o<r;o+=4)s=n.lookup[e.charCodeAt(o)],a=n.lookup[e.charCodeAt(o+1)],i=n.lookup[e.charCodeAt(o+2)],l=n.lookup[e.charCodeAt(o+3)],h[c++]=s<<2|a>>4,h[c++]=(15&a)<<4|i>>2,h[c++]=(3&i)<<6|63&l;return u}static arrayBufferToString(e){return new Promise(((t,r)=>{const n=new Blob([e]),o=new FileReader;o.addEventListener("load",(()=>t(o.result))),o.addEventListener("error",r),o.readAsText(n,"utf-8");}))}static arrayBufferToJson(e){return t(this,void 0,void 0,(function*(){return JSON.parse(yield this.arrayBufferToString(e))}))}}n.chars="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/",n.lookup=n.initLookup(),n.ArrayBufferToJson=n.arrayBufferToJson;function l(e,t="unknown"){const r=document.createElement("a");r.download=t,r.style.display="none",r.href=URL.createObjectURL(e),document.body.appendChild(r),r.click(),document.body.removeChild(r);}
-
-  /*! *****************************************************************************
-  Copyright (c) Microsoft Corporation.
-
-  Permission to use, copy, modify, and/or distribute this software for any
-  purpose with or without fee is hereby granted.
-
-  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
-  REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
-  AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
-  INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
-  LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
-  OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
-  PERFORMANCE OF THIS SOFTWARE.
-  ***************************************************************************** */
-  function e(t){return new Promise((e=>{if("number"==typeof t)setTimeout(e,t);else if("function"==typeof t){const n=setInterval((()=>{t()&&(clearInterval(n),e());}),100);}else e();}))}var a;!function(t){t.Filter="filter",t.Map="map",t.ForEach="forEach",t.Reduce="reduce",t.FlatMap="flatMap";}(a||(a={}));
+  function c(n){return new Promise(e=>{if(typeof n=="number")setTimeout(e,n);else if(typeof n=="function"){let t=setInterval(()=>{n()&&(clearInterval(t),e());},100);}else e();})}var u;(function(s){s.Filter="filter",s.Map="map",s.ForEach="forEach",s.Reduce="reduce",s.FlatMap="flatMap";})(u||(u={}));
 
   class Loading {
     
@@ -94,7 +66,7 @@
       url,
       onload(res) {
         // eslint-disable-next-line no-undef
-        l(res.response, name);
+        K(res.response, name);
         instance.hide();
       },
       onprogress(res) {
@@ -108,9 +80,7 @@
   }
 
   function getMusicName() {
-    const titleElement = document.querySelector(
-      'main .space-y-2 .inline',
-    ); 
+    const titleElement = document.querySelector('h1'); 
     return titleElement.textContent.replaceAll(' ', '')
   }
 
@@ -148,33 +118,23 @@
     text = text.toLocaleLowerCase();
     for (let [k, v] of Object.entries(textToTypeMap)) {
       if (v.every((s) => text.includes(s))) {
-        return (k ) 
+        return k 
       }
     }
     return null
   }
 
   function hideDialog() {
-    const dialogStyle = (document.querySelector(
-      '.jetstream-modal',
-    ) ).style;
+    const dialogStyle = (
+      document.querySelector('.jetstream-modal') 
+    ).style;
     dialogStyle.zIndex = '-100';
     dialogStyle.display = 'none';
     dialogStyle.position = 'fixed';
     dialogStyle.left = '-10000px';
   }
 
-  function insertCSS(href) {
-    const linkElement = document.createElement('link');
-    linkElement.rel = 'stylesheet';
-    linkElement.href = href;
-    document.head.appendChild(linkElement);
-  }
-
   function main() {
-    insertCSS(
-      'https://cdn.jsdelivr.net/npm/sweetalert2@11.1.0/dist/sweetalert2.css',
-    );
     const downloadButtonList = document.getElementsByClassName('-m-2')[1];
     hideDialog();
     downloadButtonList.addEventListener('click', async (evt) => {
@@ -182,7 +142,7 @@
       const text = target.textContent;
       const type = getType(text);
       const currLink = getLink();
-      await e(() => getLink() !== currLink);
+      await c(() => getLink() !== currLink);
       downloadMusic(getLink(), getMusicName() + '.' + typeToExtMap[type]);
       hideDialog();
     });
@@ -190,4 +150,4 @@
 
   window.addEventListener('load', main);
 
-}());
+})();
