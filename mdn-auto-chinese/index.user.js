e42ad3ba1db2b3e3e9d8e986ed2b7cd4a6cc5513
rxliuli <rxliuli@gmail.com>
2022-03-20 15:09:04 +0800

    Updates gh-pages by liuli-cli

--- mdn-auto-chinese/index.user.js
+++ mdn-auto-chinese/index.user.js
@@ -1,48 +1,26 @@
 // ==UserScript==
-// @name        mdn-auto-chinese
+// @name mdn-auto-chinese
+// @namespace https://github.com/rxliuli/userjs
+// @version 0.1.0
 // @description 如果有中文翻译的话就跳转到中文翻译页面
-// @namespace   https://github.com/rxliuli/userjs
-// @version     0.1.0
-// @author      rxliuli
-// @match       http*://developer.mozilla.org/*
-// @grant       none
+// @author rxliuli
+// @match http*://developer.mozilla.org/*
 // ==/UserScript==
 
-(function () {
-  'use strict';
-
-  /**
-   * MDN 自动跳转中文翻译
-   * 如果来到一个 MDN 的网页
-   * 判断当前页面是否为中文
-   *    - 如果是，就不进行任何操作
-   *    - 否则判断当前页面是否有中文翻译页面
-   *      - 如果没有，则不进行任何操作
-   *      - 否则跳转至中文翻译页面
-   */
+(() => {
+  // src/index.ts
+  function parseLocales() {
+    return JSON.parse(document.querySelector("#hydration").textContent).doc.other_translations.map((item) => item.locale);
+  }
   function goto() {
-    // 获取 language
-    const language = new RegExp('https://developer.mozilla.org/(.*?)/').exec(
-      location.href,
-    )[1];
-
-    // 判断 language 是否为中文
-    if (language === 'zh-CN') {
-      return
+    const language = new RegExp("https://developer.mozilla.org/(.*?)/").exec(location.href)[1];
+    if (language === "zh-CN") {
+      return;
     }
-
-    // 获取可能存在的中文翻译页面
-    const $lang = document.querySelector(
-      '#language-selector',
-    ); 
-    const someZhCN = [...$lang.options].some((item) => item.value === 'zh-CN');
-
-    // 如果有就跳转过去
+    const someZhCN = parseLocales().includes("zh-CN");
     if (someZhCN) {
-      location.href = location.href.replace(language, 'zh-CN');
+      location.href = location.href.replace(language, "zh-CN");
     }
   }
-
   goto();
-
-}());
+})();
